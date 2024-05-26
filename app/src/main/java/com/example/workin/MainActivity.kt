9aa1228fb2779e961e5ff9217492067114b427c1
Dominik Wielgus <110831391+DonixMaly@users.noreply.github.com>
2024-05-26 19:28:05 +0200

    interactive UI + sharedPreferences part 1
    UI changes upon what action is being done + working on adding SharedPreferences for records

--- app/src/main/java/com/example/workin/MainActivity.kt
+++ app/src/main/java/com/example/workin/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.workin
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
@@ -9,6 +11,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.lang.StringBuilder
@@ -25,6 +28,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val main: ConstraintLayout = findViewById(R.id.main)
 
         val homePage: Button = findViewById(R.id.HomeButton)
         val recordsPage: Button = findViewById(R.id.RecordsButton)
@@ -52,19 +57,22 @@ class MainActivity : AppCompatActivity() {
 
         val recordList: LinearLayout = findViewById(R.id.RecordList)
 
+        val sharedPreferences = getSharedPreferences("RECORDS", Context.MODE_PRIVATE)
+        var editor = sharedPreferences.edit()
+
         var timerString: String
         var recordString: String
 
         val timerStringBuilder = StringBuilder()
         val recordStringBuilder = StringBuilder()
 
-        var mostWorkOnMonday = 0
-        var mostWorkOnTuesday = 0
-        var mostWorkOnWednesday = 0
-        var mostWorkOnThursday = 0
-        var mostWorkOnFriday = 0
-        var mostWorkOnSaturday = 0
-        var mostWorkOnSunday = 0
+        var mostWorkOnMonday = sharedPreferences.getInt("MondayRecord", 0)
+        var mostWorkOnTuesday = sharedPreferences.getInt("TuesdayRecord", 0)
+        var mostWorkOnWednesday = sharedPreferences.getInt("WednesdayRecord", 0)
+        var mostWorkOnThursday = sharedPreferences.getInt("ThursdayRecord", 0)
+        var mostWorkOnFriday = sharedPreferences.getInt("FridayRecord", 0)
+        var mostWorkOnSaturday = sharedPreferences.getInt("SaturdayRecord", 0)
+        var mostWorkOnSunday = sharedPreferences.getInt("SundayRecord", 0)
 
         var timerSeconds: Int
         var timerMinutes: Int
@@ -78,22 +86,27 @@ class MainActivity : AppCompatActivity() {
         var currentDay = calendar.get(Calendar.DAY_OF_WEEK)
 
         var dayOfTheWeek = when(currentDay){
-            2 -> "Poniedziałek"
-            3 -> "Wtorek"
-            4 -> "Środa"
-            5 -> "Czwartek"
-            6 -> "Piątek"
-            0 -> "Sobota"
-            1 -> "Niedziela"
+            2 -> "Dziś jest poniedziałek"
+            3 -> "Dziś jest Wtorek"
+            4 -> "Dziś jest Środa"
+            5 -> "Dziś jest Czwartek"
+            6 -> "Dziś jest Piątek"
+            0 -> "Dziś jest Sobota"
+            1 -> "Dziś jest Niedziela"
             else -> "Brak daty"
         }
         currentDayOfWeek.text = dayOfTheWeek
+
+        timerStart.setBackgroundColor(Color.parseColor("#845AED"))
+        homePage.setBackgroundColor(Color.parseColor("#845AED"))
+        recordsPage.setBackgroundColor(Color.parseColor("#845AED"))
 
         fun SetTimer(){
             timerSeconds = countdown.toInt() / 1000
             timerMinutes = timerSeconds / 60
             timerSeconds = timerSeconds - (timerMinutes * 60)
             timerStringBuilder.append(timerMinutes).append(":").append(timerSeconds)
+            if(timerSeconds < 9) timerStringBuilder.append("0")
             timerString = timerStringBuilder.toString()
             timerStringBuilder.clear()
             bigTimer.text = timerString
@@ -106,6 +119,8 @@ class MainActivity : AppCompatActivity() {
             timerStart.visibility = View.VISIBLE
             currentDayOfWeek.visibility = View.GONE
             recordList.visibility = View.GONE
+            homePage.setBackgroundColor(Color.parseColor("#9b74fc"))
+            recordsPage.setBackgroundColor(Color.parseColor("#845AED"))
         }
         fun SetRecordsView(){
             bigTimer.visibility = View.GONE
@@ -113,6 +128,8 @@ class MainActivity : AppCompatActivity() {
             timerStart.visibility = View.GONE
             currentDayOfWeek.visibility = View.VISIBLE
             recordList.visibility = View.VISIBLE
+            recordsPage.setBackgroundColor(Color.parseColor("#9b74fc"))
+            homePage.setBackgroundColor(Color.parseColor("#845AED"))
         }
 
         fun ifBreakTime(){
@@ -139,6 +156,8 @@ class MainActivity : AppCompatActivity() {
                         in 4..7 -> medalMonday.setImageResource(R.drawable.screenshot_from_2024_03_17_12_54_47)
                         in 8..Int.MAX_VALUE -> medalMonday.setImageResource(R.drawable.cato)
                     }
+                    editor.putInt("MondayRecord", mostWorkOnMonday)
+                    editor.commit()
                 }
                 3 -> {
                     mostWorkOnTuesday += 1
@@ -224,6 +243,8 @@ class MainActivity : AppCompatActivity() {
                 isTimerRunning = false
                 timerStart.setText("START")
                 setRecord()
+                main.setBackgroundColor(Color.parseColor("#DED9FF"))
+                timerStart.setBackgroundColor(Color.parseColor("#845AED"))
             }
         }
         val breakTimer = object: CountDownTimer(300000, 1000){
@@ -235,14 +256,20 @@ class MainActivity : AppCompatActivity() {
                 ifBreakTime()
                 isTimerRunning = false
                 timerStart.setText("START")
+                main.setBackgroundColor(Color.parseColor("#DED9FF"))
+                timerStart.setBackgroundColor(Color.parseColor("#845AED"))
             }
         }
         fun startTimer(){
             if(isBreakTime == false){
                 workTimer.start()
+                main.setBackgroundColor(Color.parseColor("#FCBDDE"))
+                timerStart.setBackgroundColor(Color.parseColor("#FC5353"))
             }
             else{
                 breakTimer.start()
+                main.setBackgroundColor(Color.parseColor("#B3FCE8"))
+                timerStart.setBackgroundColor(Color.parseColor("#4CFA34"))
             }
         }
 
@@ -265,6 +292,8 @@ class MainActivity : AppCompatActivity() {
                 breakTimer.cancel()
                 ifBreakTime()
                 SetTimer()
+                main.setBackgroundColor(Color.parseColor("#DED9FF"))
+                timerStart.setBackgroundColor(Color.parseColor("#845AED"))
             }
         })
     }
